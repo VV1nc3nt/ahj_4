f946a7ecc13758d547007a3a381ea6bc69d6cb69
VV1nc3nt <gormmyz@gmail.com>
2023-09-01 00:55:42 +0300

    fix

--- e2e/cardValidator.test.js
+++ e2e/cardValidator.test.js
@@ -24,7 +24,6 @@ describe('Credit Card Validator form', () => {
       headless: false,
       slowMo: 100,
       devtools: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     page = await browser.newPage();
   });
