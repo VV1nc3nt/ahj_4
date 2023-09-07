e2aaa8dd2258545f974d38307d37869f0c3d7d50
VV1nc3nt <gormmyz@gmail.com>
2023-09-07 14:41:00 +0300

    fix

--- src/__tests__/validateCard.test.js
+++ src/__tests__/validateCard.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable prefer-const */
 /* eslint-disable import/named */
@@ -101,4 +102,43 @@ test('test undefined card', () => {
   let result = cardValidator.cardType('3');
 
   expect(result).toBe('Card undefined');
+});
+
+test('test cardImgClear func', () => {
+  document.body.innerHTML = '<form action="" class="form">' + '<input type="text" class="input_field">' + '<input type="submit" value="Проверить" class="input_submit">' + '</form>' + '<div class="card_icons">' + '<img src="./assets/img/ae.jpeg" alt="" class="card_img AmericanExpress">' + '<img src="./assets/img/dc.jpeg" alt="" class="card_img DinnersClub">' + '<img src="./assets/img/discover.jpeg" alt="" class="card_img Discover">' + '<img src="./assets/img/jcb.jpeg" alt="" class="card_img JCB">' + '<img src="./assets/img/maestro.jpeg" alt="" class="card_img Maestro">' + '<img src="./assets/img/mc.jpeg" alt="" class="card_img MasterCard">' + '<img src="./assets/img/mir.png" alt="" class="card_img MIR">' + '<img src="./assets/img/visa.jpeg" alt="" class="card_img Visa">' + '</div>';
+
+  let cardValidator = new CardValidator();
+
+  document.querySelectorAll('.card_img').forEach((el) => {
+    el.style.opacity = 1;
+  });
+  cardValidator.cardImgClear();
+
+  let element = document.querySelector('.card_img');
+  let result = window.getComputedStyle(element).getPropertyValue('opacity'); 
+
+  expect(result).toBe('0.2');
+});
+
+test('testing card img chose if card is valid', () => {
+  document.body.innerHTML = '<form action="" class="form">' + '<input type="text" class="input_field">' + '<input type="submit" value="Проверить" class="input_submit">' + '</form>' + '<div class="card_icons">' + '<img src="./assets/img/ae.jpeg" alt="" class="card_img AmericanExpress">' + '<img src="./assets/img/dc.jpeg" alt="" class="card_img DinnersClub">' + '<img src="./assets/img/discover.jpeg" alt="" class="card_img Discover">' + '<img src="./assets/img/jcb.jpeg" alt="" class="card_img JCB">' + '<img src="./assets/img/maestro.jpeg" alt="" class="card_img Maestro">' + '<img src="./assets/img/mc.jpeg" alt="" class="card_img MasterCard">' + '<img src="./assets/img/mir.png" alt="" class="card_img MIR">' + '<img src="./assets/img/visa.jpeg" alt="" class="card_img Visa">' + '</div>';
+
+  let cardValidator = new CardValidator();
+  cardValidator.cardImgChose(cardValidator.cardType('3589934582830371'));
+
+  let chosenElement = document.querySelector('.JCB');
+  let result = window.getComputedStyle(chosenElement).getPropertyValue('opacity'); 
+
+  expect(result).toBe('1');
+});
+
+test('testing card img chose if card is invalid', () => {
+  document.body.innerHTML = '<form action="" class="form">' + '<input type="text" class="input_field">' + '<input type="submit" value="Проверить" class="input_submit">' + '</form>' + '<div class="card_icons">' + '<img src="./assets/img/ae.jpeg" alt="" class="card_img AmericanExpress">' + '<img src="./assets/img/dc.jpeg" alt="" class="card_img DinnersClub">' + '<img src="./assets/img/discover.jpeg" alt="" class="card_img Discover">' + '<img src="./assets/img/jcb.jpeg" alt="" class="card_img JCB">' + '<img src="./assets/img/maestro.jpeg" alt="" class="card_img Maestro">' + '<img src="./assets/img/mc.jpeg" alt="" class="card_img MasterCard">' + '<img src="./assets/img/mir.png" alt="" class="card_img MIR">' + '<img src="./assets/img/visa.jpeg" alt="" class="card_img Visa">' + '</div>';
+
+  let cardValidator = new CardValidator();
+  cardValidator.cardImgChose(cardValidator.cardType('3'));
+
+  let result = document.contains(document.querySelector('.invalid')); 
+
+  expect(result).toBe(true);
 });
